127ec5ab47a596c30e26335b0973fc5cb819148f
Kurt <k@kcmr.io>
2018-05-02 11:45:49 -0700

    DB schema for project creation.

---
+++ data/text_analyzer_schema.sql
@@ -0,0 +1,206 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.3 (Debian 10.3-2)
+-- Dumped by pg_dump version 10.3 (Debian 10.3-2)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: kurt
+--
+
+CREATE TABLE public.categories (
+    id integer NOT NULL,
+    name character varying(25) NOT NULL
+);
+
+
+ALTER TABLE public.categories OWNER TO kurt;
+
+--
+-- Name: category_id_seq; Type: SEQUENCE; Schema: public; Owner: kurt
+--
+
+CREATE SEQUENCE public.category_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.category_id_seq OWNER TO kurt;
+
+--
+-- Name: category_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: kurt
+--
+
+ALTER SEQUENCE public.category_id_seq OWNED BY public.categories.id;
+
+
+--
+-- Name: tokens; Type: TABLE; Schema: public; Owner: kurt
+--
+
+CREATE TABLE public.tokens (
+    id integer NOT NULL,
+    phrase text NOT NULL,
+    frequency integer NOT NULL,
+    category_id integer NOT NULL
+);
+
+
+ALTER TABLE public.tokens OWNER TO kurt;
+
+--
+-- Name: tokens_id_seq; Type: SEQUENCE; Schema: public; Owner: kurt
+--
+
+CREATE SEQUENCE public.tokens_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tokens_id_seq OWNER TO kurt;
+
+--
+-- Name: tokens_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: kurt
+--
+
+ALTER SEQUENCE public.tokens_id_seq OWNED BY public.tokens.id;
+
+
+--
+-- Name: user_entries; Type: TABLE; Schema: public; Owner: kurt
+--
+
+CREATE TABLE public.user_entries (
+    id integer NOT NULL,
+    phrase text NOT NULL,
+    category_id integer NOT NULL,
+    date timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.user_entries OWNER TO kurt;
+
+--
+-- Name: user_entries_id_seq; Type: SEQUENCE; Schema: public; Owner: kurt
+--
+
+CREATE SEQUENCE public.user_entries_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.user_entries_id_seq OWNER TO kurt;
+
+--
+-- Name: user_entries_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: kurt
+--
+
+ALTER SEQUENCE public.user_entries_id_seq OWNED BY public.user_entries.id;
+
+
+--
+-- Name: categories id; Type: DEFAULT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.category_id_seq'::regclass);
+
+
+--
+-- Name: tokens id; Type: DEFAULT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.tokens ALTER COLUMN id SET DEFAULT nextval('public.tokens_id_seq'::regclass);
+
+
+--
+-- Name: user_entries id; Type: DEFAULT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.user_entries ALTER COLUMN id SET DEFAULT nextval('public.user_entries_id_seq'::regclass);
+
+
+--
+-- Name: categories category_pkey; Type: CONSTRAINT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT category_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tokens tokens_pkey; Type: CONSTRAINT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.tokens
+    ADD CONSTRAINT tokens_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_entries user_entries_pkey; Type: CONSTRAINT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.user_entries
+    ADD CONSTRAINT user_entries_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tokens tokens_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.tokens
+    ADD CONSTRAINT tokens_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+
+--
+-- Name: user_entries user_entries_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: kurt
+--
+
+ALTER TABLE ONLY public.user_entries
+    ADD CONSTRAINT user_entries_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
