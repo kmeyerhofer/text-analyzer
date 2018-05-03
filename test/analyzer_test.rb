9e2913b5d421c6e0aa7e56031d1596a85fd682c1
Kurt <k@kcmr.io>
2018-05-03 11:55:58 -0700

    Tests remove entries from DB using teardown method.

--- test/analyzer_test.rb
+++ test/analyzer_test.rb
@@ -7,13 +7,39 @@ require_relative '../analyzer'
 class AnalyzeTest < Minitest::Test
   include Rack::Test::Methods
 
+  def connection
+    PG.connect(dbname: 'text_analyzer_dev')
+  end
+
+  def setup
+    @time = Time.now
+    @phrase = ''
+  end
+
+  def teardown
+    remove_from_db(@phrase)
+  end
+
+  def remove_from_db(phrase)
+    year = @time.year
+    month = @time.month
+    day = @time.day
+    hour = @time.hour
+    min = @time.min
+    sql = <<~SQL
+    DELETE FROM user_entries WHERE phrase = $1 AND
+    EXTRACT(YEAR FROM date) = $2 AND
+    EXTRACT(MONTH FROM DATE) = $3 AND
+    EXTRACT(DAY FROM DATE) = $4 AND
+    EXTRACT(HOUR FROM DATE) = $5 AND
+    EXTRACT(MINUTE FROM DATE) = $6;
+    SQL
+    connection.exec_params(sql, [phrase, year, month, day, hour, min])
+  end
+
   def app
     Sinatra::Application
   end
-
-  # def setup
-  #   # Dir.entries('./data').include?('combined-lexicon.yml')
-  # end
 
   def session
     last_request.env['rack.session']
@@ -27,14 +53,14 @@ class AnalyzeTest < Minitest::Test
   end
 
   def test_text_analyze_negative
-    post '/result', text_to_analyze: 'testing this text'
+    post '/result', text_to_analyze: @phrase = 'testing this text'
     assert_equal 200, last_response.status
     assert_includes last_response.body, 'testing this text'
     assert_includes last_response.body, "Negative"
   end
 
   def test_text_analyze_positive
-    post '/result', text_to_analyze: 'testing'
+    post '/result', text_to_analyze: @phrase = 'testing'
     assert_equal 200, last_response.status
     assert_includes last_response.body, 'testing'
     assert_includes last_response.body, "Positive"
@@ -58,46 +84,37 @@ class AnalyzeTest < Minitest::Test
     assert_equal 200, last_response.status
   end
 
-  def test_recent_entries
-    text1 = "the train to denver" # positive
-    text2 = "you missed it!" # negative
-    post '/result', text_to_analyze: text1
-    post '/result', text_to_analyze: text2
+  def test_recent_entries_with_result_class
+    @phrase = 'wheat' # positive
+    post '/result', text_to_analyze: @phrase
+    assert_includes last_response.body, 'Positive'
     get '/'
-    assert_includes last_response.body, text1
-    assert_includes last_response.body, text2
+    assert_includes last_response.body, @phrase
+    assert_includes last_response.body, "class=\"positive\""
   end
 
   def test_clear_recent_entries
-    text1 = "the train to denver" # positive
-    text2 = "you missed it!" # negative
-    post '/result', text_to_analyze: text1
-    post '/result', text_to_analyze: text2
+    @phrase = 'you missed it!' # negative
+    post '/result', text_to_analyze: @phrase
+    assert_includes last_response.body, 'Negative'
     get '/'
-    assert_includes last_response.body, text1
-    assert_includes last_response.body, text2
+    assert_includes last_response.body, @phrase
+    assert_includes last_response.body, "class=\"negative\""
     post '/clear'
-    refute_includes last_response.body, text1
-    refute_includes last_response.body, text2
+    refute_includes last_response.body, @phrase
   end
 
   def test_database_categories
-    test = Lexicon.new
-    assert_equal ['positive', 'negative'], test.words.data.data.keys
+    assert_equal ['positive', 'negative'], Lexicon.new.words.data.data.keys
   end
 
   def test_database_token_count
-    test = Lexicon.new
-    connection = PG.connect(dbname: 'text_analyzer_dev')
-
     db_tokens = connection.exec('SELECT count(*) FROM tokens')[0]['count']
-    assert_equal db_tokens.to_i, test.words.vocab.tokens.count
+    assert_equal db_tokens.to_i, Lexicon.new.words.vocab.tokens.count
   end
 
   def test_database_category_count
-    test = Lexicon.new
-    connection = PG.connect(dbname: 'text_analyzer_dev')
     db_categories = connection.exec('SELECT count(*) FROM categories')[0]['count']
-    assert_equal db_categories.to_i, test.words.data.data.keys.count
+    assert_equal db_categories.to_i, Lexicon.new.words.data.data.keys.count
   end
 end
