172b640c2af7aa160d740343a895b093b7f2744e
Kurt <k@kcmr.io>
2018-06-03 17:12:38 -0700

    Begins refactorization with creation of an InputAnalysis class.

--- analyzer.rb
+++ analyzer.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
-require_relative './lib/lexicon'
+# require_relative './lib/lexicon'
 require_relative './lib/random_sentence'
-require_relative './lib/dbconnect'
+# require_relative './lib/dbconnect'
+require_relative './lib/input_analysis'
 
 
 require 'pry'
@@ -20,47 +21,46 @@ before do
     session[:input].delete_at(-1)
   end
 
-  @lexicon = Lexicon.new(db_name)
+  # @lexicon = Lexicon.new(db_name)
 end
 
 helpers do
-  def db_name
-    ENV["DATABASE_NAME"]
-  end
+  # def db_name
+  #   ENV["DATABASE_NAME"]
+  # end
 
   def clean_text(text)
     Rack::Utils.escape_html(text).gsub(/\r/, '')
-
   end
 
-  def css(result)
-    max_value = result.values.max
-    max_result = result.key(max_value)
-    max_result == 'positive' ? max_result : 'negative'
-  end
+  # def css(result)
+  #   max_value = result.values.max
+  #   max_result = result.key(max_value)
+  #   max_result == 'positive' ? max_result : 'negative'
+  # end
 
-  def save_user_entry(text, result)
-    db = DBConnect.new(db_name)
-    db.user_entry(text, result)
-  end
-
-  def format_percent(results)
-    results.map {|num| format("%.3f", (num * 100)) + '%'}
-  end
-
-  def format_single_line_results(results_hash)
-
-  end
-
-  def format_list_item_string(results_hash)
-    array = []
-    results_hash.each_pair do |phrase, data|
-      array << "<span class=\"#{data[2]}\" title=\"#{data[0]} positive, #{data[1]} negative\">#{phrase}</span>"
-    end
-    array.unshift("<li>")
-    array.push("</li>")
-    array.join('')
-  end
+  # def save_user_entry(text, result)
+  #   db = DBConnect.new(db_name)
+  #   db.user_entry(text, result)
+  # end
+  #
+  # def format_percent(results)
+  #   results.map {|num| format("%.3f", (num * 100)) + '%'}
+  # end
+  #
+  # def format_single_line_results(results_hash)
+  #
+  # end
+  #
+  # def format_list_item_string(results_hash)
+  #   array = []
+  #   results_hash.each_pair do |phrase, data|
+  #     array << "<span class=\"#{data[2]}\" title=\"#{data[0]} positive, #{data[1]} negative\">#{phrase}</span>"
+  #   end
+  #   array.unshift("<li>")
+  #   array.push("</li>")
+  #   array.join('')
+  # end
 end
 
 get '/' do
@@ -68,32 +68,28 @@ get '/' do
 end
 
 post '/result' do
-  # binding.pry
+  text_to_analyze_length = params[:text_to_analyze].size
   @cleaned_up_text = clean_text(params[:text_to_analyze].to_s.strip)
-  if @cleaned_up_text.size == 0
+  input = InputAnalysis.new(@cleaned_up_text) # remove the instance variable?
+
+  if text_to_analyze_length == 0
     session[:flash_message] = 'Please input text.'
     redirect to '/'
 
-  elsif params[:text_to_analyze].size >= 750
+  elsif text_to_analyze_length >= 750
     session[:flash_message] = 'Please input less than 750 characters.'
     redirect to '/'
 
   elsif params[:analysis_type] == 'all_text'
-    # binding.pry
-    @result = @lexicon.raw_data(@cleaned_up_text)
-    @cssresult = css(@result)
-    @positive_percent, @negative_percent = format_percent(@result.values)
-    # binding.pry
-    session[:input].unshift(format_list_item_string(@cleaned_up_text =>
-      [@positive_percent, @negative_percent, @cssresult]))
-    save_user_entry(params[:text_to_analyze], @cssresult)
+    @view_data = input.view_elements
+    session[:input].unshift(input.session_elements)
     erb :result
 
   elsif params[:analysis_type] == 'punctuation_delimiter'
     # @result = @lexicon.raw_data(@cleaned_up_text)
-    @punctuation_separated_text = @cleaned_up_text.split(/(?<=[?.!,])/)
+    punctuation_separated_text = @cleaned_up_text.split(/(?<=[?.!,])/)
     @separated_results = {}
-    @punctuation_separated_text.each do |phrase|
+    punctuation_separated_text.each do |phrase|
       results = @lexicon.raw_data(phrase)
       @separated_results[phrase] = format_percent(results.values) << css(results)
     end
@@ -102,9 +98,9 @@ post '/result' do
 
   elsif params[:analysis_type] == 'new_line'
     # @result = @lexicon.raw_data(@cleaned_up_text)
-    @new_line_separated_text = @cleaned_up_text.split(/\n/)
+    new_line_separated_text = @cleaned_up_text.split(/\n/)
     @separated_results = {}
-    @new_line_separated_text.each do |phrase|
+    new_line_separated_text.each do |phrase|
       results = @lexicon.raw_data(phrase)
       @separated_results[phrase] = format_percent(results.values) << (css(results))
     end
@@ -116,6 +112,8 @@ end
 
 get '/random' do
   random_result = RandomSentence.new
+  input = InputAnalysis.new(random_result, random_result.source)
+
   @cleaned_up_text = random_result.selection
   @text_source = random_result.source
   @result = @lexicon.raw_data(@cleaned_up_text)
---
+++ lib/input_analysis.rb
@@ -0,0 +1,67 @@
+require_relative './dbconnect'
+require_relative './lexicon'
+
+class InputAnalysis
+  attr_reader :cleaned_text, :lexicon, :analysis_result, :random_text_source,
+              :positive_percent, :negative_percent, :background_css_class
+
+  def initialize(cleaned_text, text_source = nil)
+    @cleaned_text = cleaned_text
+    @lexicon = Lexicon.new(db_name)
+    @random_text_source = text_source
+    @analysis_result = text_analysis
+    @positive_percent, @negative_percent = format_percent
+    @background_css_class = css_class
+  end
+
+  def save_user_entry(text)
+    db = DBConnect.new(db_name)
+    db.user_entry(text, background_css_class)
+  end
+
+  def session_elements
+    format_list_item_string(cleaned_text =>
+      [positive_percent, negative_percent, background_css_class])
+  end
+
+  def view_elements
+    view_data = {}
+    view_data[:cleaned_up_text] = cleaned_text
+    view_data[:css_result] = background_css_class
+    view_data[:positive_percent] = positive_percent
+    view_data[:negative_percent] = negative_percent
+    view_data[:text_source] = random_text_source
+
+    view_data
+  end
+
+  private
+
+  def text_analysis
+    lexicon.raw_data(cleaned_text)
+  end
+
+  def css_class
+    max_value = analysis_result.values.max
+    max_result = analysis_result.key(max_value)
+    max_result == 'positive' ? max_result : 'negative'
+  end
+
+  def format_percent
+    analysis_result.values.map { |num| format("%.3f", (num * 100)) + '%' }
+  end
+
+  def format_list_item_string(results_hash)
+    array = []
+    results_hash.each_pair do |phrase, data|
+      array << "<span class=\"#{data[2]}\" title=\"#{data[0]} positive, #{data[1]} negative\">#{phrase}</span>"
+    end
+    array.unshift("<li>")
+    array.push("</li>")
+    array.join('')
+  end
+
+  def db_name
+    ENV['DATABASE_NAME']
+  end
+end
