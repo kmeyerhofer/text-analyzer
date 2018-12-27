d235e7936ee16f94a06d93e5554d0e7acae8a4fc
Kurt Meyerhofer <k@kcmr.io>
2018-12-26 19:55:06 -0700

    Adds error handling.

--- public/javascript/vueTextAnalyzer.js
+++ public/javascript/vueTextAnalyzer.js
@@ -32,22 +32,50 @@ var app = new Vue({
           text_to_analyze: self.text_to_analyze
         },
       }).then(function(response) {
-        self.results.unshift(response.data);
-      });
+        if (response.data.error) {
+          self.results.unshift([response.data]);
+        } else {
+          self.results.unshift(response.data);
+        }
+      }).catch(function(error) {
+        self.results.unshift(self.formatError(error.message));
+      })
     },
     formatResult: function(resultArr) {
       let result = '';
-      for (let i = 0; i < resultArr.length; i += 1) {
-        result += "<span ";
-        result += "class=\"" + resultArr[i].css_result + "\"";
-        result += "title=\"" + resultArr[i].positive_percent + " positive, ";
-        result += resultArr[i].negative_percent + " negative\">";
-        result += resultArr[i].text
-        result += "</span>"
-        if (i != resultArr.length - 1) {
-          result += " "
+      if (resultArr[0].error) {
+        result += this.listError(resultArr[0]);
+      } else {
+        for (let i = 0; i < resultArr.length; i += 1) {
+          result += this.listResult(resultArr[i]);
+          if (i != resultArr.length - 1) {
+            result += " ";
+          }
         }
       }
+      return result;
+    },
+    listResult: function(resultItem) {
+      let result = '';
+      result += "<span ";
+      result += "class=\"" + resultItem.css_result + "\"";
+      result += "title=\"" + resultItem.positive_percent + " positive, ";
+      result += resultItem.negative_percent + " negative\">";
+      result += resultItem.text;
+      result += "</span>";
+      return result;
+    },
+    listError: function(error) {
+      let result = '';
+      result += "<span ";
+      result += "class=\"negative\">";
+      result += error.error;
+      result += "</span>";
+      return result;
+    },
+    formatError: function(message) {
+      let result = [];
+      result.push({ "error": message });
       return result;
     },
     clearEntries: function() {
